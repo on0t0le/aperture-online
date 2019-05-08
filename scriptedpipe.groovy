61b3eb863be791450ce66e0bdc67c4687c46181a
unknown <bugrovka12@ukr.net>
2019-05-08 11:34:34 +0300

    WS and Cleanup

--- scriptedpipe.groovy
+++ scriptedpipe.groovy
@@ -5,9 +5,22 @@ node('slave1'){
         checkout scm
     }
 
-    stage('Build Docker'){
+    stage('List Docker'){
         def containerList = sh(script: 'docker ps', returnStdout: true)
         echo "You have this containers"
         echo "${containerList}"
+    }
+
+    stage('List workspace'){
+        def wsList = sh(script:'ls -la',returnStdout:true)
+        echo "Workspace contains: ${wsList}"
+    }
+
+    stage('Cleanup'){
+        sh 'docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
+        sh 'docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
+        sh 'docker image ls -q | xargs --no-run-if-empty docker image rm -f'
+        sh 'docker volume ls -qf dangling=true | xargs -r docker volume rm'
+        deleteDir()
     }
 }
