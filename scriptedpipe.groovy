062ca108efca9822b2983d208e4b7858863a96cc
unknown <bugrovka12@ukr.net>
2019-05-08 11:44:38 +0300

    Deploy add

--- scriptedpipe.groovy
+++ scriptedpipe.groovy
@@ -16,6 +16,16 @@ node('slave1'){
         echo "Workspace contains: ${wsList}"
     }
 
+    stage('Docker build'){
+        sh 'docker build -t myregistry.com/admin/webapp .'
+    }
+
+    stage('Deploy to registry'){
+        withDockerRegistry(credentialsId: '61581225-5c57-46e5-9e9b-ddd07ac0806b', url: 'myregistry.com:5000') {
+            sh 'docker push myregistry.com/admin/webapp'
+        }
+    }
+
     stage('Cleanup'){
         sh 'docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
         sh 'docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
